b033362213abe8f666cb314f2e812f8456c5a7e3
Mauricio Fragale <mfragale@gmail.com>
2025-03-07 10:44:44 -0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -4,12 +4,12 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="bg-white p-3 shadow-md w-full md:w-[90%] lg:w-[70%] max-w-4xl md:rounded-lg">
-      <h1 className="text-2xl lg:text-4xl font-bold text-primary tracking-wider uppercase text-center py-2">
+    <div className="bg-white shadow-md p-3 md:rounded-lg w-full md:w-[90%] lg:w-[70%] max-w-4xl">
+      <h1 className="py-2 font-bold text-primary text-2xl lg:text-4xl text-center uppercase tracking-wider">
         Welcome to Quizy
       </h1>
       <Separator />
-      <div className="grid grid-cols-1 md:grid-cols-2 p-2 md:px-6 py-3 gap-4">
+      <div className="gap-4 grid grid-cols-1 md:grid-cols-2 p-2 md:px-6 py-3">
         <div className="relative h-full">
           <Image
             src={"/hero.webp"}
@@ -17,7 +17,7 @@ export default function Home() {
             priority
             width={450}
             height={450}
-            className="object-cover object-center mx-auto"
+            className="mx-auto object-center object-cover"
           />
         </div>
         <QuizSettings />
